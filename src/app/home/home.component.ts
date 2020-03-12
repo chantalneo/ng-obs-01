f14f6e1b06218c7086bc3c51d94775d52ae01f01
Chantal Neo <Chantal@hotmail.sg>
2020-03-13 00:31:53 +0800

    Errors and Completion

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -16,16 +16,30 @@ export class HomeComponent implements OnInit, OnDestroy {
     // this.firstObsSubscription = interval(1000).subscribe(count => {
     //   console.log(count);
     // })
-    const customIntervalObservable = Observable.create((observable) => {
+    const customIntervalObservable = Observable.create((observer) => {
       let count = 0;
       setInterval(() => {
-        observable.next(count++);
+        observer.next(count);
+        if (count === 2) {
+          observer.complete();
+        }        
+        if (count > 3) {
+          observer.error(new Error("Count is greater than 3!"));
+        }
+        count++;
       }, 1000);
     });
 
     this.firstObsSubscription = customIntervalObservable.subscribe(
-      (data) => {
+      data => {
         console.log(data);
+      },
+      error => { // An error would cancel the observable
+        console.log(error);
+        alert(error.message);
+      },
+      () => {
+        console.log("Completed!")
       }
     );
   }
